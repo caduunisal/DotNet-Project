740a4b5b96d2575fdf4b9fba9e53250657c5e39e
caduunisal <kadu.unisal1@gmail.com>
2024-11-06 15:32:17 -0300

    Improving the app

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a13cf311af64f70f22440cc68beda913f9f57392")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d3c2c016827e28c3593c65d81edc5f3c0d7b4fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
